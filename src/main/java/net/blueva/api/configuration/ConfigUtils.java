5fd6717f9a90d4ebfa6310838695c17aff7cc9c7
Whiron <dnlblasco@gmail.com>
2023-08-12 04:41:36 +0200

    Fixed bug with 'ConfigUtils'

--- src/main/java/net/blueva/api/configuration/ConfigUtils.java
+++ src/main/java/net/blueva/api/configuration/ConfigUtils.java
@@ -3,7 +3,7 @@ package net.blueva.api.configuration;
 import java.io.File;
 
 public class ConfigUtils {
-    public void generateFolder(String folderName, File dataFolder) {
+    public static void generateFolder(String folderName, File dataFolder) {
         File folder = new File(dataFolder+"/"+folderName);
         if(!folder.exists()) {
             folder.mkdirs();
